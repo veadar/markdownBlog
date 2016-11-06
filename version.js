358a3ba1f8707098ec04e7ba2dc60e7f73ec5f5f
veadar <veadar@gmail.com>
2016-11-06 13:41:25 +0900

    テキストが長いとファイル保存時に内容が途切れてしまう問題の修正

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 {
   "markdownBlog": {
-    "version": "1.1.1",
+    "version": "1.1.2",
     "url": "https://github.com/veadar/markdownBlog/releases",
-    "comment": "・0埋め2桁の分が変換されない不具合を修正"
+    "comment": "・テキストが長いとファイル保存時に内容が途切れてしまう問題の修正"
   }
 }
